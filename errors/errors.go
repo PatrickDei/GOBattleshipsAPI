daa2b1dbe8d378e7f3b70f2a90a314498178e808
Patrick Deisinger <patrick.deis@gmail.com>
2022-09-12 10:38:19 +0200

    Player handler test coverage

--- errors/errors.go
+++ errors/errors.go
@@ -2,30 +2,44 @@ package errors
 
 import "net/http"
 
+const ERROR_BODY_ERROR_CODE = "error-code"
+const ERROR_BODY_ERROR_ARG = "error-arg"
+
+const ERROR_CODE_PREFIX = "error."
+
+type ErrorBody map[string]string
+
+func NewErrorBody(code string, arg string) ErrorBody {
+	return map[string]string{
+		ERROR_BODY_ERROR_CODE: code,
+		ERROR_BODY_ERROR_ARG:  arg,
+	}
+}
+
 type AppError struct {
 	Code int
-	Body map[string]string
+	Body ErrorBody
 }
 
 func (ae AppError) AsResponseMessage() map[string]string {
 	return ae.Body
 }
 
-func NewConflictError(body map[string]string) *AppError {
+func NewConflictError(body ErrorBody) *AppError {
 	return &AppError{
 		Code: http.StatusConflict,
 		Body: body,
 	}
 }
 
-func NewNotFoundError(body map[string]string) *AppError {
+func NewNotFoundError(body ErrorBody) *AppError {
 	return &AppError{
 		Code: http.StatusNotFound,
 		Body: body,
 	}
 }
 
-func NewInternalServerError(body map[string]string) *AppError {
+func NewInternalServerError(body ErrorBody) *AppError {
 	return &AppError{
 		Code: http.StatusInternalServerError,
 		Body: body,
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/PatrickDei/log-lib v1.1.0 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/golang/mock v1.6.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/jmoiron/sqlx v1.3.5 // indirect
 	go.uber.org/atomic v1.7.0 // indirect

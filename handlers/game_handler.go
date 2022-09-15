1f31c8b1f05d8cb9a08fe8a3b101a73c16a34909
Patrick Deisinger <patrick.deis@gmail.com>
2022-09-15 11:30:43 +0200

    Get game state handler layer

--- handlers/game_handler.go
+++ handlers/game_handler.go
@@ -10,6 +10,7 @@ import (
 
 const gameLocation = "game/match-"
 const OpponentPathParam = "opponent_id"
+const GamePathParam = "game_id"
 
 type GameHandler struct {
 	Facade service.GameFacade
@@ -31,5 +32,18 @@ func (gh GameHandler) ChallengePlayer(w http.ResponseWriter, r *http.Request) {
 			w.Header().Add("Location", gameLocation+game.Id)
 			writeResponse(w, http.StatusCreated, game)
 		}
+	}
+}
+
+func (gh GameHandler) GetGameState(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	playerId := vars[PlayerPathParam]
+	gameId := vars[GamePathParam]
+
+	gs, err := gh.Facade.GetGameStatus(playerId, gameId)
+	if err != nil {
+		writeResponse(w, err.Code, err.AsResponseMessage())
+	} else {
+		writeResponse(w, http.StatusCreated, gs)
 	}
 }
